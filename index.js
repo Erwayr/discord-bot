14be39ca2206483bbe473abb31a36e76bb296e33
Erwayr <erwayrcelen0@gmail.com>
2025-07-06 18:14:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ client.on(Events.GuildMemberAdd, async (member) => {
 client.on(Events.MessageCreate, async (message) => {
   if (!message.guild || message.author.bot) return;
 
-  await electionHandler(message, db, LOG_CHANNEL_ID);
+  await electionHandler(message, db, GENERAL_CHANNEL_ID);
   if (message.content.trim() === "!rank") {
     await rankHandler(message, db);
   }
