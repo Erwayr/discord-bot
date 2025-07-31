824d152872d690f2cbee359e6490b5d0b50538e4
Erwayr <erwayrcelen0@gmail.com>
2025-07-31 14:37:36 +0200

    fsdf

--- script/messageCountHandler.js
+++ script/messageCountHandler.js
@@ -1,30 +1,23 @@
-// script/messageCountHandler.js
-const userDocRefs = new Map(); // discord_id -> ref Firestore
-
+const userDocRefs = new Map();
+const admin = require("firebase-admin");
 module.exports = async function messageCountHandler(message, db) {
   try {
     const discordId = message.author.id;
-
     let userRef = userDocRefs.get(discordId);
 
-    // Si on n'a pas déjà la référence du doc Firestore
     if (!userRef) {
       const snap = await db
         .collection("followers_all_time")
         .where("discord_id", "==", discordId)
         .limit(1)
         .get();
-
       if (snap.empty) {
         console.warn(`⚠️ Aucun document trouvé pour discord_id = ${discordId}`);
         return;
       }
-
       userRef = snap.docs[0].ref;
-      userDocRefs.set(discordId, userRef); // on garde juste la ref
+      userDocRefs.set(discordId, userRef);
     }
-
-    // Évite la lecture Firestore, on utilise FieldValue.increment
     await userRef.update({
       discord_count_message: admin.firestore.FieldValue.increment(1),
     });
